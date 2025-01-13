78a86e390fcde7446705fcc285b7d9aac707ad4f
ShenalGanegoda <106686816+ShenalGanegoda@users.noreply.github.com>
2025-01-13 09:38:07 +0530

    systemChooseInput method start

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static Scanner scanner = new Scanner(System.in); // Global scanner for reading user inputs.
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        systemChooseInput();
+    }
+
+    private static char systemChooseInput(){
+
+        /*
+        This section need to be run in a loop till the user inputs a valid input
+         */
+
+        boolean correctInput = false;
+
+        System.out.println("******* University of Oxford progression outcome system *******");
+        System.out.println();
+
+
+        // Prompting the user selection of the system.
+        System.out.println("University admin - 'U' \nStudent - 'S' ");
+        System.out.print("Enter here: ");
+        char systemInput = scanner.next().toLowerCase().charAt(0); // Storing the user input in a String variable.
+
+        return systemInput;
     }
 }
